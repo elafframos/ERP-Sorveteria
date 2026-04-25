fc4bbd133623d5257c57e71e53c2b05bee8e8abd
elafframos <elaff.dev@gmail.com>
2026-04-25 13:00:49 -0300

    Configurando ALLOWED_HOSTS para o Railway

--- Project/settings.py
+++ Project/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['erp-sorveteria-production.up.railway.app', '127.0.0.1', 'localhost']
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
